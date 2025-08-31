999a1b9c6297e4e151039a22676d684d26f2bc33
JonatanFD <u20211b995@upc.edu.pe>
2025-08-31 02:17:34 -0500

    feat: Add documentation for locale handling in paths module

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,6 +1,15 @@
 import { Locale } from "./i18n";
 import { ChallengeEditStep } from "./types";
+/*
+    Note: Not always locale parameter will be asked. It depends on the route.
+    Based on the middleware configuration, the route loses its query params if locale is not provided.
+    In case you need to remain the query params you need to pass the locale parameter.
 
+    You can see PATHS.CHALLENGES.EDIT.STEP function as example.
+    Be careful with the first '/' in the path, this indicates the root of the application.
+    If it is not provided then the path will be relative to the current page.
+    If it is provided then the path will be absolute.
+*/
 const PATHS = {
     HOME: "/",
     LOGIN: "/auth/login",
